a8bf0c7d900ee4b78f866b4e3872f71243064b8e
Pramod H C <pramodhc007@gmail.com>
2026-08-27 02:55:24 +0530

    adds toast controller to show notice or alert

---
+++ app/javascript/controllers/toast_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+import * as bootstrap from "bootstrap"
+
+class ToastController extends Controller {
+    connect() {
+        const toast = bootstrap.Toast.getOrCreateInstance(this.element)
+        toast.show()
+    }
+
+    hide() {
+        bootstrap.Toast.getOrCreateInstance(this.element).hide()
+    }
+}
+
+export default ToastController
