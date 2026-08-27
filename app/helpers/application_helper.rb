a84902d92a35499b4494ab70431b0050bcffd0d1
Pramod H C <pramodhc007@gmail.com>
2026-08-27 10:47:01 +0530

    resolves safepass font issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def logo(size = "h2")
-    link_to(root_path, class: "logo#{size}") do
+    link_to(root_path, class: "logo #{size}") do
       "<i class=\"bi bi-safe-fill me-2\"></i>SafePass".html_safe
     end
   end
