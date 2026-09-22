49bada0bf13bdc6bb5ffb790576bc741c1bbf70f
Pramod H C <pramodhc007@gmail.com>
2026-09-22 15:26:46 +0530

    resolves entry create issue

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,15 +1,23 @@
 class EntriesController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create ]
+
   def new
     @entry = Entry.new
   end
 
   def create
-    @entry = current_user.entries.build(entry_params)
+    @entry = current_user.entries.new(entry_params)
     if @entry.save
       redirect_to root_path, flash: { notice: "Entry created successfully." }
     else
       flash[:alert] = "Sorry, there was an error creating your entry. Please check the form and try again."
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def entry_params
+    params.expect(entry: [ :name, :url, :username, :password ])
   end
 end
