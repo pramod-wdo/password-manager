e00ba4b8c092b750b9ddfd1ef10a94135833d1f1
Pramod H C <pramodhc007@gmail.com>
2026-09-22 15:30:57 +0530

    adds validation and custom validations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,17 @@
 class Entry < ApplicationRecord
   belongs_to :user
+  validates :name, :username, :password, presence: true
+  validates :url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp }
+  validate :url_must_be_valid
+
+  private
+
+  def url_must_be_valid
+    uri = URI.parse(url)
+    unless uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+      errors.add(:url, "must be a valid HTTP or HTTPS URL")
+    end
+  rescue URI::InvalidURIError
+    errors.add(:url, "must be a valid URL")
+  end
 end
