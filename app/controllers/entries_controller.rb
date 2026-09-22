2ef267c76b7fdd7009bfcac24b31dfd1673bfc02
Pramod H C <pramodhc007@gmail.com>
2026-09-22 09:21:41 +0530

    completed create action

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,4 +2,14 @@ class EntriesController < ApplicationController
   def new
     @entry = Entry.new
   end
+
+  def create
+    @entry = current_user.entries.build(entry_params)
+    if @entry.save
+      redirect_to root_path, flash: { notice: "Entry created successfully." }
+    else
+      flash[:alert] = "Sorry, there was an error creating your entry. Please check the form and try again."
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
